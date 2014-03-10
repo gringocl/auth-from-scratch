022298bfd636a09cfc6bd047617434d11d4c0057
Miles Starkenburg <milesstarkenburg@gmail.com>
2014-03-10 16:53:41 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuthFromScratch::Application.config.secret_key_base = 'bde979b2f9aa6a2a4e48832cc4bf943565577ce073bbd3d321228a22adb0c476315e9b19d7abb4e4c6c73d15782910ca210394933fac4baaed1366255292f93f'
